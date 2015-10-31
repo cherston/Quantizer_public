1f8b78efdaaeaa227d6c069f9486aedbe538fa1c
cherston <cherston@mit.edu>
2015-10-31 18:37:30 -0400

    tweaks

--- py/check_file.py
+++ py/check_file.py
@@ -1,10 +1,14 @@
 def checker(fname):
 	with open(fname, 'rb') as fh: 
 		first = next(fh).decode()
-		fh.seek(-1024,2)
+		try: 
+			fh.seek(-1024,2)
+		except: 
+			print "file invalid"
+			return False 
 		last = fh.readlines()[-1].decode()
 		print last 
-		if last == "</Event>\n" or last == "</Event>": 
+		if last == "</Event>\n" or last == "</Event>" and fname.endswith('.xml'): 
 			return True
 		else:
 			print "file invalid"
